ab4df9c40e2172e48b00d80449038ac2b444e078
Duy Hoàng <duypo03cbvn@gmail.com>
2026-02-05 00:32:29 +0700

    fix svhn classes keyword

--- coreset/coresetmethod.py
+++ coreset/coresetmethod.py
@@ -3,7 +3,10 @@ class CoresetMethod(object):
         if fraction <= 0.0 or fraction > 1.0:
             raise ValueError("Illegal Coreset Size.")
         self.dst_train = dst_train
-        self.num_classes = len(dst_train.classes)
+        if hasattr(dst_train, 'classes'):
+            self.num_classes = len(dst_train.classes)
+        else:
+            self.num_classes = args.num_classes
         self.fraction = fraction
         self.random_seed = random_seed
         self.index = []
